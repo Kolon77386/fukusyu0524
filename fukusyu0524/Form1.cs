73cbac30122b549af7d669401004478fa67d7089
ookamiyoujyo <ookamiyoujyo@gmail.com>
2019-05-24 13:11:09 +0900

    復習完成

--- fukusyu0524/Form1.cs
+++ fukusyu0524/Form1.cs
@@ -12,9 +12,42 @@ namespace fukusyu0524
 {
     public partial class Form1 : Form
     {
+        int vx;
+        int vy;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = -10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vx = -10;
+            vy = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vx = 10;
+            vy = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = 10;
         }
     }
 }
